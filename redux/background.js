105c5eedc8146ac5b59dd2f931109c96930f2a69
Wez Furlong <wez@netevil.org>
2011-01-01 13:00:57 -0500

    fix whitespace, no functional changes

--- redux/background.js
+++ redux/background.js
@@ -1,3 +1,6 @@
+/* vim:noet:ts=4:sw=4
+ */
+
 var default_settings = {
 	style: 'style-newspaper',
 	size: 'size-large',
@@ -14,31 +17,31 @@ function createJavascript(settings)
 {
 	var js_url, css_url, print_url;
 
-    if (settings.remote && !settings.enable_experimental) {
+	if (settings.remote && !settings.enable_experimental) {
 		var arc90 = "http://lab.arc90.com/experiments/readability/";
-        js_url = arc90 + "js/readability.js?x="+(Math.random());
-        css_url = arc90 + "/css/readability.css";
-        print_url = arc90 + "/css/readability-print.css";
-    } else {
-      if (settings.enable_experimental)
-      {
-          js_url = chrome.extension.getURL('readability/readability-x.js');
-      } else {
-          js_url = chrome.extension.getURL('readability/readability.js');
-      }
+		js_url = arc90 + "js/readability.js?x="+(Math.random());
+		css_url = arc90 + "/css/readability.css";
+		print_url = arc90 + "/css/readability-print.css";
+	} else {
+		if (settings.enable_experimental)
+		{
+			js_url = chrome.extension.getURL('readability/readability-x.js');
+		} else {
+			js_url = chrome.extension.getURL('readability/readability.js');
+		}
 
-      css_url = chrome.extension.getURL('readability/readability.css');
-      print_url = chrome.extension.getURL('readability/readability-print.css');
-    }
+		css_url = chrome.extension.getURL('readability/readability.css');
+		print_url = chrome.extension.getURL('readability/readability-print.css');
+	}
 
-    var code = "(function(){readConvertLinksToFootnotes=" + settings['enable_footnotes'] + ";readStyle='" + settings['style'] + "';readSize='" + settings['size'] + "';readMargin='" + settings['margin'] + "';_readability_script=document.createElement('SCRIPT');_readability_script.type='text/javascript';_readability_script.src='" + js_url + "';document.getElementsByTagName('head')[0].appendChild(_readability_script);_readability_css=document.createElement('LINK');_readability_css.rel='stylesheet';_readability_css.href='" + css_url + "';_readability_css.type='text/css';_readability_css.media='screen';document.getElementsByTagName('head')[0].appendChild(_readability_css);_readability_print_css=document.createElement('LINK');_readability_print_css.rel='stylesheet';_readability_print_css.href='" + print_url + "';_readability_print_css.media='print';_readability_print_css.type='text/css';document.getElementsByTagName('head')[0].appendChild(_readability_print_css);})();";
+	var code = "(function(){readConvertLinksToFootnotes=" + settings['enable_footnotes'] + ";readStyle='" + settings['style'] + "';readSize='" + settings['size'] + "';readMargin='" + settings['margin'] + "';_readability_script=document.createElement('SCRIPT');_readability_script.type='text/javascript';_readability_script.src='" + js_url + "';document.getElementsByTagName('head')[0].appendChild(_readability_script);_readability_css=document.createElement('LINK');_readability_css.rel='stylesheet';_readability_css.href='" + css_url + "';_readability_css.type='text/css';_readability_css.media='screen';document.getElementsByTagName('head')[0].appendChild(_readability_css);_readability_print_css=document.createElement('LINK');_readability_print_css.rel='stylesheet';_readability_print_css.href='" + print_url + "';_readability_print_css.media='print';_readability_print_css.type='text/css';document.getElementsByTagName('head')[0].appendChild(_readability_print_css);})();";
 
-    return code;
+	return code;
 }
 
 function render(tab_id)
 {
-    chrome.tabs.sendRequest(tab_id, {'type': 'render'});
+	chrome.tabs.sendRequest(tab_id, {'type': 'render'});
 }
 
 function getSettings()
@@ -51,7 +54,7 @@ function getSettings()
 		}
 	}
 
-    return settings;
+	return settings;
 }
 
 function setSettings(settings)
@@ -69,40 +72,40 @@ function setSettings(settings)
 	}
 //	console.log('Settings now: ' + JSON.stringify(localStorage));
 
-    chrome.windows.getAll({'populate': true}, function(windows)
-    {
-        _.each(windows, function(window)
-        {
-            _.each(window.tabs, function(tab)
-            {
-                chrome.tabs.sendRequest(tab.id, {'type': 'newSettings', 'settings': getSettings()});
-            });
-        });
-    });
+	chrome.windows.getAll({'populate': true}, function(windows)
+	{
+		_.each(windows, function(window)
+		{
+			_.each(window.tabs, function(tab)
+			{
+				chrome.tabs.sendRequest(tab.id, {'type': 'newSettings', 'settings': getSettings()});
+			});
+		});
+	});
 }
 
 function requestHandler(data, sender, callback)
 {
 	var result = undefined;
 
-    if (data['type'] == 'javascript') {
-        if (_.include(_.keys(data), 'settings'))
-            result = createJavascript(data['settings']);
-        else
-            result = createJavascript(getSettings());
-    }
+	if (data['type'] == 'javascript') {
+		if (_.include(_.keys(data), 'settings'))
+			result = createJavascript(data['settings']);
+		else
+			result = createJavascript(getSettings());
+	}
 
-    if (data['type'] == 'setSettings') {
-        setSettings(data['settings']);
-    }
+	if (data['type'] == 'setSettings') {
+		setSettings(data['settings']);
+	}
 
-    if (data['type'] == 'getSettings') {
-        result = getSettings();
-    }
+	if (data['type'] == 'getSettings') {
+		result = getSettings();
+	}
 
-    if (data['type'] == 'render') {
-        render(data['tab_id']);
-    }
+	if (data['type'] == 'render') {
+		render(data['tab_id']);
+	}
 
 	callback(result);
 }
@@ -112,6 +115,6 @@ chrome.extension.onRequestExternal.addListener(requestHandler);
 
 chrome.browserAction.onClicked.addListener(function(tab)
 {
-    render(tab.id);
+	render(tab.id);
 });
 
--- redux/options.js
+++ redux/options.js
@@ -1,245 +1,247 @@
+/* vim:noet:ts=4:sw=4
+ */
 
 var settings = {
 
-    init: function()
-    {
-        $('select, input').change(_.bind(function() {
+	init: function() {
+		$('select, input').change(_.bind(function() {
 			this.preview();
 			this.markDirty();
 		}, this));
 
-        $('#cancel').click(_.bind(this.load, this));
-        $('#save').click(_.bind(this.save, this));
+		$('#cancel').click(_.bind(this.load, this));
+		$('#save').click(_.bind(this.save, this));
 
-        this.load();
-    },
+		this.load();
+	},
 
-    getSelect: function(name)
-    {
-        return $('#' + name).val();
-    },
+	getSelect: function(name)
+	{
+		return $('#' + name).val();
+	},
 
-    setSelect: function(name, value)
-    {
-        if($('#' + name + ' option[value=' + value + ']')
-           .attr('selected', 'selected')
-           .val() === undefined)
-           $('#' + name + ' option:first-child').attr('selected', 'selected');
-    },
+	setSelect: function(name, value)
+	{
+		if ($('#' + name + ' option[value=' + value + ']')
+				.attr('selected', 'selected')
+				.val() === undefined) {
+			$('#' + name + ' option:first-child').attr('selected', 'selected');
+		}
+	},
 
-    markDirty: function()
-    {
-        $('#save').attr('disabled', '');
-    },
+	markDirty: function()
+	{
+		$('#save').attr('disabled', '');
+	},
 
-    markClean: function()
-    {
-        $('#save').attr('disabled', 'disabled');
-    },
+	markClean: function()
+	{
+		$('#save').attr('disabled', 'disabled');
+	},
 
-    save: function()
-    {
-        var settings = {
-            style: this.getSelect('r_style'),
-            size: this.getSelect('r_size'),
-            margin: this.getSelect('r_margin'),
-            enable_footnotes: $('#enable_footnotes').attr('checked'),
-            enable_experimental: $('#enable_experimental').attr('checked'),
-            enable_keys: $('#enable_keys').attr('checked'),
-            remote: $('#remote').attr('checked'),
-            keys: keybox.keys
-        };
+	save: function()
+	{
+		var settings = {
+			style: this.getSelect('r_style'),
+			size: this.getSelect('r_size'),
+			margin: this.getSelect('r_margin'),
+			enable_footnotes: $('#enable_footnotes').attr('checked'),
+			enable_experimental: $('#enable_experimental').attr('checked'),
+			enable_keys: $('#enable_keys').attr('checked'),
+			remote: $('#remote').attr('checked'),
+			keys: keybox.keys
+		};
 
-        //console.log(settings);
+		//console.log(settings);
 
-        chrome.extension.sendRequest(
-            {'type': 'setSettings', 'settings': settings},
-            _.bind(this.markClean, this));
-    },
+		chrome.extension.sendRequest(
+			{'type': 'setSettings', 'settings': settings},
+			_.bind(this.markClean, this));
+	},
 
-    load: function()
-    {
-        chrome.extension.sendRequest({'type': 'getSettings'}, _.bind(function(settings)
-        {
-            this.setSelect('r_style', settings['style']);
-            this.setSelect('r_size', settings['size']);
-            this.setSelect('r_margin', settings['margin']);
-            $('#enable_footnotes').attr('checked', settings['enable_footnotes']);
+	load: function()
+	{
+		chrome.extension.sendRequest({'type': 'getSettings'}, _.bind(function(settings)
+		{
+			this.setSelect('r_style', settings['style']);
+			this.setSelect('r_size', settings['size']);
+			this.setSelect('r_margin', settings['margin']);
+			$('#enable_footnotes').attr('checked', settings['enable_footnotes']);
 
-            keybox.keys = settings['keys'];
-            if(settings['enable_keys'])
-                keybox.enable();
-            else
-                keybox.disable();
+			keybox.keys = settings['keys'];
+			if(settings['enable_keys'])
+				keybox.enable();
+			else
+				keybox.disable();
 
 			$('#enable_experimental').attr('checked',
 				settings['enable_experimental']);
 
 			$('#remote').attr('checked', settings['remote']);
 
-            keybox.update();
-            this.preview()
-            this.markClean();
-        }, this));
-    },
+			keybox.update();
+			this.preview()
+			this.markClean();
+		}, this));
+	},
 
-    /* This is a bit wicked, but doing plain simple
-     * location = 'javascript:...' resulted in blank iframe.
-     * (this is used in redux/example.html for the preview)
-     */
-    hello_from_child: function(preview_window)
-    {
-        this.preview_window = preview_window;
-        this.preview();
-    },
+	/* This is a bit wicked, but doing plain simple
+		* location = 'javascript:...' resulted in blank iframe.
+		* (this is used in redux/example.html for the preview)
+		*/
+	hello_from_child: function(preview_window)
+	{
+		this.preview_window = preview_window;
+		this.preview();
+	},
 
-    preview_window: null,
+	preview_window: null,
 
-    preview: function()
-    {
-        var settings = {
-            style: this.getSelect('r_style'),
-            size: this.getSelect('r_size'),
-            margin: this.getSelect('r_margin'),
-            enable_footnotes: $('#enable_footnotes').attr('checked'),
-            remote: $('#remote').attr('checked')
-        };
+	preview: function()
+	{
+		var settings = {
+			style: this.getSelect('r_style'),
+			size: this.getSelect('r_size'),
+			margin: this.getSelect('r_margin'),
+			enable_footnotes: $('#enable_footnotes').attr('checked'),
+			remote: $('#remote').attr('checked')
+		};
 
-        chrome.extension.sendRequest({'type': 'javascript', 'settings': settings}, _.bind(function(js)
-        {
-            if(this.preview_window !== null)
-            {
-                this.preview_window.inject(js);
-            }
-        }, this));
-    }
+		chrome.extension.sendRequest({'type': 'javascript', 'settings': settings}, _.bind(function(js)
+		{
+			if(this.preview_window !== null)
+			{
+				this.preview_window.inject(js);
+			}
+		}, this));
+	}
 };
 
 
 var keybox = {
-    pressed: 0,
-    keys: [],
-    enabled: false,
+	pressed: 0,
+	keys: [],
+	enabled: false,
 
-    init: function()
-    {
-        $('#keys').keydown(_.bind(this.keydown, this));
-        $('#keys').keyup(_.bind(this.keyup, this));
-        $('#keys').focus(_.bind(this.focus, this));
-        $('#keys').blur(_.bind(this.blur, this));
-        $('#enable_keys').change(_.bind(this.checkbox, this));
-    },
+	init: function()
+	{
+		$('#keys').keydown(_.bind(this.keydown, this));
+		$('#keys').keyup(_.bind(this.keyup, this));
+		$('#keys').focus(_.bind(this.focus, this));
+		$('#keys').blur(_.bind(this.blur, this));
+		$('#enable_keys').change(_.bind(this.checkbox, this));
+	},
 
-    enable: function()
-    {
-        $('#enable_keys').attr('checked', 'checked');
-        $('#keys').css('background-color', '');
-        $('#keys').css('color', '');
-        this.enabled = 1;
-    },
+	enable: function()
+	{
+		$('#enable_keys').attr('checked', 'checked');
+		$('#keys').css('background-color', '');
+		$('#keys').css('color', '');
+		this.enabled = 1;
+	},
 
-    disable: function()
-    {
-        $('#enable_keys').attr('checked', '');
-        $('#keys').css('background-color', 'silver');
-        $('#keys').css('color', 'gray');
-        this.enabled = 0;
-    },
+	disable: function()
+	{
+		$('#enable_keys').attr('checked', '');
+		$('#keys').css('background-color', 'silver');
+		$('#keys').css('color', 'gray');
+		this.enabled = 0;
+	},
 
-    checkbox: function()
-    {
-        if($('#enable_keys').attr('checked'))
-            this.enable();
-        else
-            this.disable();
-    },
+	checkbox: function()
+	{
+		if($('#enable_keys').attr('checked'))
+			this.enable();
+		else
+			this.disable();
+	},
 
-    // It sometimes fails, like when Ctrl+Shift+U, U has a strange key code 229. Dunno why.
-    keydown: function(e)
-    {
-        if(!this.enabled)
-            return;
+	// It sometimes fails, like when Ctrl+Shift+U, U has a strange key code 229. Dunno why.
+	keydown: function(e)
+	{
+		if(!this.enabled)
+			return;
 
-        if(!((e.which >= 65 && e.which <= 90) || e.which == 16 || e.which == 17 || e.which == 18))
-            return false;
+		if(!((e.which >= 65 && e.which <= 90) || e.which == 16 || e.which == 17 || e.which == 18))
+			return false;
 
-        if(this.pressed == 0)
-            this.keys = [];
+		if(this.pressed == 0)
+			this.keys = [];
 
-        if(_.include(this.keys, e.which)) // Degenerate situation
-        {
-            this.keys = [];
-            this.pressed = 0;
-        }
+		if(_.include(this.keys, e.which)) // Degenerate situation
+		{
+			this.keys = [];
+			this.pressed = 0;
+		}
 
-        this.keys.push(e.which);
-        this.pressed++;
+		this.keys.push(e.which);
+		this.pressed++;
 
-        settings.markDirty();
-        this.update();
-    },
+		settings.markDirty();
+		this.update();
+	},
 
-    keyup: function(e)
-    {
-        if(!this.enabled)
-            return;
+	keyup: function(e)
+	{
+		if(!this.enabled)
+			return;
 
-        if(!((e.which >= 65 && e.which <= 90) || e.which == 16 || e.which == 17 || e.which == 18))
-            return false;
+		if(!((e.which >= 65 && e.which <= 90) || e.which == 16 || e.which == 17 || e.which == 18))
+			return false;
 
-        this.pressed--;
+		this.pressed--;
 
-        this.update();
-    },
+		this.update();
+	},
 
-    update: function()
-    {
-        var value = [];
-        this.keys.sort();
+	update: function()
+	{
+		var value = [];
+		this.keys.sort();
 
-        for(var i = 0; i < this.keys.length; i++)
-        {
-            var key = this.keys[i];
-            if(key >= 65 && key <= 90)
-                value.push(String.fromCharCode(key));
+		for(var i = 0; i < this.keys.length; i++)
+		{
+			var key = this.keys[i];
+			if(key >= 65 && key <= 90)
+				value.push(String.fromCharCode(key));
 
-            if(key == 16)
-                value.push("Shift");
+			if(key == 16)
+				value.push("Shift");
 
-            if(key == 17)
-                value.push("Ctrl");
+			if(key == 17)
+				value.push("Ctrl");
 
-            if(key == 18)
-                value.push("Alt");
-        }
+			if(key == 18)
+				value.push("Alt");
+		}
 
-        $("#keys").attr('value', value.join(' + '));
-    },
+		$("#keys").attr('value', value.join(' + '));
+	},
 
-    focus: function()
-    {
-        if(!this.enabled)
-            return;
+	focus: function()
+	{
+		if(!this.enabled)
+			return;
 
-        $('#keys').css('background-color', '#ebeff9');
-    },
+		$('#keys').css('background-color', '#ebeff9');
+	},
 
-    blur: function()
-    {
-        if(!this.enabled)
-            return;
+	blur: function()
+	{
+		if(!this.enabled)
+			return;
 
-        $('#keys').css('background-color', '');
-        $('#keys').css('color', '');
-    }
+		$('#keys').css('background-color', '');
+		$('#keys').css('color', '');
+	}
 };
 
 $(document).ready(function()
 {
-    $('#example iframe').ready(function()
-    {
-        settings.init();
-        keybox.init();
-    });
+	$('#example iframe').ready(function()
+	{
+		settings.init();
+		keybox.init();
+	});
 });
 
