2609279c0a4cd8de288cea134c663b646eb466ee
Geoff Greer <geoff@greer.fm>
2012-07-03 03:18:02 -0700

    Actually fix defaults. Expose show_article_tools in readability.js

--- redux/background.js
+++ redux/background.js
@@ -24,7 +24,7 @@ function createJavascript(settings)
       var print_url = chrome.extension.getURL('readability/readability-print.css');
     }
 
-    var code = "(function(){readConvertLinksToFootnotes=" + settings['enable_links'] + ";readStyle='" + settings['style'] + "';readSize='" + settings['size'] + "';readMargin='" + settings['margin'] + "';_readability_script=document.createElement('SCRIPT');_readability_script.type='text/javascript';_readability_script.src='" + js_url + "';document.getElementsByTagName('head')[0].appendChild(_readability_script);_readability_css=document.createElement('LINK');_readability_css.rel='stylesheet';_readability_css.href='" + css_url + "';_readability_css.type='text/css';_readability_css.media='screen';document.getElementsByTagName('head')[0].appendChild(_readability_css);_readability_print_css=document.createElement('LINK');_readability_print_css.rel='stylesheet';_readability_print_css.href='" + print_url + "';_readability_print_css.media='print';_readability_print_css.type='text/css';document.getElementsByTagName('head')[0].appendChild(_readability_print_css);})();";
+    var code = "(function(){showArticleTools=" + settings['show_article_tools'] + ";readConvertLinksToFootnotes=" + settings['enable_links'] + ";readStyle='" + settings['style'] + "';readSize='" + settings['size'] + "';readMargin='" + settings['margin'] + "';_readability_script=document.createElement('SCRIPT');_readability_script.type='text/javascript';_readability_script.src='" + js_url + "';document.getElementsByTagName('head')[0].appendChild(_readability_script);_readability_css=document.createElement('LINK');_readability_css.rel='stylesheet';_readability_css.href='" + css_url + "';_readability_css.type='text/css';_readability_css.media='screen';document.getElementsByTagName('head')[0].appendChild(_readability_css);_readability_print_css=document.createElement('LINK');_readability_print_css.rel='stylesheet';_readability_print_css.href='" + print_url + "';_readability_print_css.media='print';_readability_print_css.type='text/css';document.getElementsByTagName('head')[0].appendChild(_readability_print_css);})();";
 
     return code;
 }
@@ -75,7 +75,14 @@ function getSettings()
         keys: []
     };
 
-    return _.extend(settings, defaults);
+    // Kill all keys with undefined values, so defaults can take over
+    _.each(settings, function (value, setting) {
+      if(_.isUndefined(value)) {
+        delete settings[setting];
+      }
+    })
+
+    return _.extend(defaults, settings);
 }
 
 function setSettings(settings)
