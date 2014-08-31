839e86582d4f9c314f5c58435db9d0a232ea43e9
Geoff Greer <geoff@greer.fm>
2014-08-31 15:08:45 -0700

    Remove google analytics from extension.

--- redux/background.js
+++ redux/background.js
@@ -162,16 +162,6 @@ chrome.browserAction.onClicked.addListener(function(tab)
     render(tab.id);
 });
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-27938065-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
- var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- ga.src = 'https://ssl.google-analytics.com/ga.js';
- var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 /*chrome.contextMenus.create({
     'title': 'Readability',
     'contexts': ['page', 'selection'],
